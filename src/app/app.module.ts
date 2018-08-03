5e20cb5143608bfad14bc5a54e9ebefa03b020be
Saswat Mohanty <saswat.mohanty@imaginea.com>
2018-08-03 15:56:11 +0530

    redux devtool added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ColumnComponent } from './column/column.component';
 import { DatatableComponent } from './datatable/datatable.component';
-import { NgRedux, NgReduxModule } from '@angular-redux/store';
+import { NgRedux, NgReduxModule, DevToolsExtension } from '@angular-redux/store';
 import { IAppState, rootReducer, INITIAL_STATE } from './redux/store';
 import { StateService } from './services/state.service';
 
@@ -24,7 +24,15 @@ import { StateService } from './services/state.service';
 })
 export class AppModule {
 
-  constructor (ngRedux: NgRedux<IAppState>) {
-    ngRedux.configureStore(rootReducer, INITIAL_STATE);
+  constructor (ngRedux: NgRedux<IAppState>, private devTools: DevToolsExtension) {
+    let enhancers = [];
+    // ... add whatever other enhancers you want.
+
+    // You probably only want to expose this tool in devMode.
+    if (devTools.isEnabled()) {
+      enhancers = [ ...enhancers, devTools.enhancer() ];
+    }
+
+    ngRedux.configureStore(rootReducer, INITIAL_STATE, [], enhancers);
   }
 }
