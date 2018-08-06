fdb249d0889b57b81633de0e7d8e41d0a81ff3d2
Saswat Mohanty <saswat.mohanty@imaginea.com>
2018-08-06 14:42:18 +0530

    protractor html report added

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -7,23 +7,35 @@ describe('workspace-project App', () => {
   beforeEach(() => {
     page = new AppPage();
     page.navigateTo();
+    browser.executeScript('window.localStorage.clear()');
+  });
+
+  afterEach(() => {
+    browser.executeScript('window.localStorage.clear()');
   });
 
   it('should display div container', () => {
-    // page.navigateTo();
     expect(page.getParagraphText()).toEqual('container');
   });
 
   it('click add button', () => {
-    // page.navigateTo();
     page.getAddButton().click();
     expect(page.getModalWindow()).toEqual('addNewModal');
   });
 
-  // it('add new item', () => {
-  //   page.getAddButton().click();
-  //   expect(page.getModalWindow()).toEqual('addNewModal');
+  it('add new item', () => {
+    page.getAddButton().click();
 
-  // });
+    element(by.name('inputName')).sendKeys('Bob');
+    element(by.name('address')).sendKeys('Hyderabad');
+    element(by.name('age')).sendKeys('25');
+    element(by.name('selectStatus')).sendKeys('active');
+
+    element(by.css('input[type=submit]')).click();
+
+    const list = element.all(by.className('tr-item-list'));
+    expect(list.count()).toEqual(1);
+
+  });
 
 });
